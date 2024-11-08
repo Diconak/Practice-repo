1df5654e01d40be115a594111a84cffd8ac4198f
Diconak <dimitrijevranic@yahoo.com>
2024-11-09 00:20:49 +0100

    Added a few comprehensive comments to the code, preparing to make docker.compose

--- Web/Webstore/Basket.API/Entities/ShoppingCart.cs
+++ Web/Webstore/Basket.API/Entities/ShoppingCart.cs
@@ -4,6 +4,7 @@
     {
 
         public string Username { get; set; }
+        //Moramo ovde da dodamo ovo get i set, da bi mogli da pristupimo Entitetu za ShoppingCartItem u schemi
         public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
         public decimal TotalPrice; 
         public ShoppingCart(string username = null) { 
--- Web/Webstore/Basket.API/Program.cs
+++ Web/Webstore/Basket.API/Program.cs
@@ -8,9 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Radili smo dependency injection za interface
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddControllers();
 
+//Ovo nam povezuje nas servis, i konfigurise ga, u nasem slucaju, povezuje nas servis na port gde nam se nalazi redis
 builder.Services.AddStackExchangeRedisCache(
     opts => {
         opts.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
