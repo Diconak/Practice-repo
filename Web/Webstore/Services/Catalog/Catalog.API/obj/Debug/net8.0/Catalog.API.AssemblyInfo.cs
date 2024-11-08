d0e668c8ec08e9c8f450242cbe00084edebc87fd
Diconak <dimitrijevranic@yahoo.com>
2024-11-08 15:54:34 +0100

    Added Basket.API to project

--- Web/Webstore/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ Web/Webstore/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac6a5831e98865403537d162252c23df1924c871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982c1fe9af66b323adffc348b4fc6eb0dccfe831")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
