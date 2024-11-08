844d4c1d908e021083bcd79650bf373fd38778cc
Diconak <dimitrijevranic@yahoo.com>
2024-11-08 19:09:59 +0100

    Added repository, controller, and entities to Basket.API

---
+++ Web/Webstore/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items = new List<ShoppingCartItem>();
+        public decimal TotalPrice; 
+        public ShoppingCart(string username = null) { 
+            Username = username ?? throw new ArgumentNullException(nameof(username)); 
+        }
+    }
+}
